f774d698edbae68bd218ed802d15ba5a09e1f97e
paragdangui <33660969+paragdangui@users.noreply.github.com>
2024-11-05 14:53:46 +0530

    Section 4: Error Handling(client and server side)

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
+	defineProps({
+		car: Object,
+	});
 	const route = useRoute();
 	const { toTitleCase } = useUtilities();
+	const { cars } = useCars();
 	useHead({
 		title: `${toTitleCase(route.params.name)}`,
 	});
@@ -8,13 +12,27 @@
 	definePageMeta({
 		layout: 'custom',
 	});
+
+	const car = computed(() => {
+		return cars.find((c) => {
+			return c.id === parseInt(route.params.id);
+		});
+	});
+
+	if (!car.value) {
+		throw createError({
+			statusCode: 404,
+			statusMessage: `Car with id of ${route.params.id} deesn't exist`,
+			fatal: true,
+		});
+	}
 </script>
 
 <template>
-	<div>
-		<CarDetailHero />
-		<CarDetailAttributes />
-		<CarDetailDescription />
+	<div v-if="car">
+		<CarDetailHero :car="car" />
+		<CarDetailAttributes :features="car.features" />
+		<CarDetailDescription :description="car.description" />
 		<CarDetailContact />
 	</div>
 </template>
