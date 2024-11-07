72d1cf7978b018c901157a0f18d744abbc46d921
paragdangui <33660969+paragdangui@users.noreply.github.com>
2024-11-07 19:08:50 +0530

    Section6: completed filters.

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -6,7 +6,24 @@
 	});
 
 	const city = ref('');
+	const priceRange = ref({
+		min: '',
+		max: '',
+	});
+	const priceRangeText = computed(() => {
+		const minPrice = route.query.minPrice;
+		const maxPrice = route.query.maxPrice;
+
+		if (!minPrice && !maxPrice) return 'Any';
+		else if (!minPrice && maxPrice) return `< $${maxPrice}`;
+		else if (minPrice && !maxPrice) return `> $${maxPrice}`;
+		else {
+			return `${minPrice} - ${maxPrice}`;
+		}
+	});
 	const route = useRoute();
+	const router = useRouter();
+	const { makes } = useCars();
 
 	const updateModal = (key: string) => {
 		modal.value[key] = !modal.value[key];
@@ -23,6 +40,24 @@
 		updateModal('location');
 		navigateTo(`/city/${city.value}/car/${route.params.make}`);
 		city.value = '';
+	};
+
+	const onChangeMake = (make: string) => {
+		updateModal('make');
+		navigateTo(`/city/${route.params.city}/car/${make}`);
+	};
+
+	const onChangePrice = () => {
+		updateModal('price');
+		if (priceRange.value.max && priceRange.value.min) {
+			if (priceRange.value.min > priceRange.value.max) return;
+		}
+		router.push({
+			query: {
+				minPrice: priceRange.value.min,
+				maxPrice: priceRange.value.max,
+			},
+		});
 	};
 </script>
 
@@ -50,13 +85,54 @@
 		<div class="p-5 flex justify-between relative cursor-pointer border-b">
 			<h3>Make</h3>
 			<h3 class="text-blue-400 capitalize" @click="updateModal('make')">
-				Toyota
+				{{ route.params.make || 'Any' }}
 			</h3>
+
+			<div
+				v-if="modal.make"
+				class="absolute border shadow left-56 p-5 top-1 -m-1 w-[600px] flex justify-between flex-wrap bg-white"
+			>
+				<h4
+					v-for="make in makes"
+					:key="make"
+					class="w-1/3"
+					@click="onChangeMake(make)"
+				>
+					{{ make }}
+				</h4>
+			</div>
 		</div>
 
 		<div class="p-5 flex justify-between relative cursor-pointer border-b">
 			<h3 @click="updateModal('price')">Price</h3>
-			<h3 class="text-blue-400 capitalize"></h3>
+			<h3 class="text-blue-400 capitalize" @click="updateModal('price')">
+				{{ priceRangeText }}
+			</h3>
+			<div
+				v-if="modal.price"
+				class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white"
+			>
+				<input
+					type="number"
+					placeholder="Min"
+					v-model="priceRange.min"
+					class="border p-1 rounded"
+				/>
+
+				<input
+					type="number"
+					placeholder="Max"
+					v-model="priceRange.max"
+					class="border p-1 rounded"
+				/>
+
+				<button
+					class="bg-blue-400 w-full mt-2 rounded text-white p-1"
+					@click="onChangePrice"
+				>
+					Apply
+				</button>
+			</div>
 		</div>
 	</div>
 </template>
